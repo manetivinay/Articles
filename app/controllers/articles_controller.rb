ec4dc41b420181e7fc9b0e4ef81ead55474af70c
Vinay Maneti <vinaymaneti@gmail.com>
2016-06-25 04:08:41 +0700

    save

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,11 @@ class ArticlesController < ApplicationController
 
 
   def index
-    @articles = Article.all
+    if params[:search]
+      @articles = Article.search(params[:search]).order("created_at DESC")
+    else
+      @articles = Article.order("created_at DESC")
+    end
   end
 
   def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -9,4 +9,10 @@ class Article < ActiveRecord::Base
   validates :text,
             presence: true,
             length: {minimum: 5}
+
+  # It returns the articles whose titles contain one or more words that form the query
+  def self.search(query)
+    # where(:title, query) -> This would return an exact match of the query
+    where("title like ?", "%#{query}%")
+  end
 end
